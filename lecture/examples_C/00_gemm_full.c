a34ea70b8f0170818d60963d0f11b8186c0d2a9a
Petr Karpov <61159523+karpov-peter@users.noreply.github.com>
2023-10-02 15:05:23 +0300

    Create 00_gemm_full.c

---
+++ lecture/examples_C/00_gemm_full.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include "cblas.h"
+
+int main() 
+{
+int i, j;
+
+// Define matrices A, B, and C
+double A[6] = {1.0, 2.0, 1.0, 4.0, 5.0, 6.0}; // 3x2 matrix
+double B[6] = {1.0, 2.0, 3.0, 4.0, 5.0, 6.0}; // 2x3 matrix
+double C[9] = {0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0}; // 3x3 matrix
+
+// Parameters for dgemm
+int m = 3; // Number of rows in matrices A and C
+int n = 3; // Number of columns in matrices B and C
+int k = 2; // Number of columns in matrix A; number of rows in matrix B
+double alpha = 1.0;
+double beta = 0.0;
+int lda = 2; // Leading dimension of matrix A
+int ldb = 3; // Leading dimension of matrix B
+int ldc = 3; // Leading dimension of matrix C
+
+// Perform matrix multiplication: C = alpha * A * B + beta * C
+cblas_dgemm(CblasRowMajor, CblasNoTrans, CblasNoTrans, m, n, k, alpha, A, lda, B, ldb, beta, C, ldc);
+
+// Print the resulting C matrix
+printf("Resulting C matrix:\n");
+for (i = 0; i < m; i++) 
+    {
+    for (j = 0; j < n; j++) 
+        printf("%f ", C[i * n + j]);
+  
+    printf("\n");
+    }
+}
+
+return 0;
+}
