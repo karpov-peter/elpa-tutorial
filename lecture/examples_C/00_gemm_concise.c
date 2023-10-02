fbf615148f0545b50ee9f7ffb403c041c6d3a6b5
Petr Karpov <61159523+karpov-peter@users.noreply.github.com>
2023-10-02 15:08:09 +0300

    Update 00_gemm_concise.c

--- lecture/examples_C/00_gemm_concise.c
+++ lecture/examples_C/00_gemm_concise.c
@@ -1,11 +1,12 @@
 #include "cblas.h"
 
-int main() {
-    double a[6] = {1, 2, 1, 4, 5, 6}; 
-    double b[6] = {1, 2, 3, 4, 5, 6}; 
-    double c[9] = {0}; 
+int main() 
+{
+double a[6] = {1, 2, 1, 4, 5, 6}; 
+double b[6] = {1, 2, 3, 4, 5, 6}; 
+double c[9] = {0}; 
 
-    cblas_dgemm(CblasRowMajor, CblasNoTrans, CblasNoTrans, 3, 3, 2, 1.0, a, 2, b, 3, 0.0, c, 3);
+cblas_dgemm(CblasRowMajor, CblasNoTrans, CblasNoTrans, 3, 3, 2, 1.0, a, 2, b, 3, 0.0, c, 3);
 
-    return 0;
+return 0;
 }
