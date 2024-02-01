f6813ed859ba3c4ddfe884fb0e2d602aaa40c240
Petr Karpov <petr.karpov@mpcdf.mpg.de>
2024-02-01 18:28:19 +0100

    Add the Raven job script for examples_C/04_eigenproblem_elpa

--- examples_C/04_eigenproblem_elpa/eigenproblem_elpa.c
+++ examples_C/04_eigenproblem_elpa/eigenproblem_elpa.c
@@ -27,7 +27,7 @@ enum matrix_types matrix_type = Symmetrized_Clement;
 int ELPA_SOLVER = ELPA_SOLVER_2STAGE;
 
 //__________________________________________________________________________________
-// blacs
+// BLACS routines
 void   Cblacs_pinfo( int* mypnum, int* world_size);
 void   Cblacs_get( int context, int request, int* value);
 int    Cblacs_gridinit( int* context, char * order, int np_row, int np_col);
@@ -35,17 +35,12 @@ void   Cblacs_gridinfo( int context, int*  np_row, int* np_col, int*  my_row, in
 void   Cblacs_gridexit( int context);
 void   Cblacs_exit( int error_code);
 
-// scalapack
+// ScaLAPACK routines (Fortran-style, hence the trailing underscore is needed)
 int    numroc_( int *n, int *NB, int *iproc, int *isrcproc, int *world_size);
 void   descinit_( int *desc, int *m, int *n, int *mb, int *NB, int *irsrc, int *icsrc, int *ictxt, int *lld, int *info);
 
 //__________________________________________________________________________________
 
-
-int Setup_ELPA()
-	{
-	return 0;	
-	}
 
 double get_global_matrix_element (int I_gl, int J_gl)
    {
@@ -77,32 +72,30 @@ double get_global_matrix_element (int I_gl, int J_gl)
 int main(int argc, char **argv) 
 {
 //____________________________________________ 
-// setup MPI
+// Setup MPI
 int world_rank, world_size; // MPI
 
-// for hybrid MPI+OpenMP
+// For hybrid MPI+OpenMP
 //int thread_level;
 //MPI_Init_thread(&argc, &argv, MPI_THREAD_FUNNELED, &thread_level); 
 
+// For pure MPI
 MPI_Init( &argc, &argv);
+
 MPI_Comm_size(MPI_COMM_WORLD, &world_size);
 MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
 
 //____________________________________________ 
 
-//int world_rank, world_size; // blacs
 int ictxt, nprow, npcol, myrow, mycol;
 int np, nq;
 int m_loc, n_loc;
 int info, itemp, seed, lwork;
 int descA[9], descZ[9];
 int izero=0;
-//double *A_loc; // this is the matrix we diagonalize here
-//double *Z_loc, *work, *Eigenvalues;
-  
-
 
 //____________________________________________ 
+// Parse command line arguments
 
 if (argc==1) // one argument was provided: filename (default)
    {
@@ -110,19 +103,19 @@ if (argc==1) // one argument was provided: filename (default)
    nev = N;
    }
    
-if (argc==2) // two arguments were provided: filename (default), N
+if (argc==2) // two arguments were provided: filename (default), N (matrix size)
    {
    N   = atoi(argv[1]);
    nev = N;
    }
 
-if (argc==3) // three arguments were provided: filename (default), N, nev
+if (argc==3) // three arguments were provided: filename (default), N (matrix size), nev
    {
    N   = atoi(argv[1]);
    nev = atoi(argv[2]);
    }
 
-if (argc==4) // four arguments were provided: filename (default), N, nev, NB
+if (argc==4) // four arguments were provided: filename (default), N (matrix size), nev, NB ()
    {
    N   = atoi(argv[1]);
    nev = atoi(argv[2]);
@@ -130,6 +123,7 @@ if (argc==4) // four arguments were provided: filename (default), N, nev, NB
    }
 
 //____________________________________________ 
+// Determine the grid size
 
 nprow = sqrt(world_size); 
 npcol = sqrt(world_size); 
@@ -141,9 +135,6 @@ if (world_size==20) {nprow=4; npcol=5;}
 if (world_size==40) {nprow=5; npcol=8;}
 
 if (world_size==72) {nprow=8; npcol=9;}
-//if (world_size==72) {nprow=9; npcol=8;}
-//if (world_size==72) {nprow=6; npcol=12;}
-//if (world_size==72) {nprow=12; npcol=6;}
 
 if (world_size==72*4) {nprow=8*2; npcol=9*2;}
 
@@ -162,13 +153,13 @@ if (world_rank==0) printf("world_size=%i, nprow=%i, npcol=%i, N=%i, nev=%i, NB=%
 
 //____________________________________________ 
 	
-// setup Blacs
+// Setup BLACS
 Cblacs_pinfo( &world_rank, &world_size ) ;
 Cblacs_get( -1, 0, &ictxt );
-Cblacs_gridinit( &ictxt, "Row", nprow, npcol );
+Cblacs_gridinit( &ictxt, "Row", nprow, npcol ); // "Row" or "Col" is the ordering of the processes in the grid. ELPA works with either of them
 Cblacs_gridinfo( ictxt, &nprow, &npcol, &myrow, &mycol );
 
-// Compute the size of the local matrices (thanks to numroc)
+// Compute the size of the local matrices A_loc, Z_loc (thanks to numroc)
 m_loc = numroc_( &N, &NB, &myrow, &izero, &nprow );
 n_loc = numroc_( &N, &NB, &mycol, &izero, &npcol );
 if (debug_mode) printf("myrow=%i, mycol=%i, m_loc=%i, n_loc=%i, \n", myrow, mycol, m_loc, n_loc);
@@ -179,21 +170,23 @@ if (debug_mode) printf("myrow=%i, mycol=%i, m_loc=%i, n_loc=%i, \n", myrow, myco
 ///*
 int error, status;
 elpa_t handle;
-   
-if (elpa_init(20170403) != ELPA_OK) 
+
+status = elpa_init(20170403);   
+if (status != ELPA_OK) 
 	{
 	fprintf(stderr, "Error: ELPA API version not supported");
+  MPI_Finalize();
 	exit(1);
 	}
 
 handle = elpa_allocate(&error);
 assert_elpa_ok(error);
 
-//elpa_set(handle, "na", n, &error); // matrix dimension
+// Set ELPA mandatory parameters. This can be done only once per ELPA object (handle)
 elpa_set_integer(handle, "na", N, &error); // matrix dimension
 if (world_rank==0 || debug_mode) printf("na=%i is set, error=%i\n", N, error);
 
-elpa_set_integer(handle, "nev", nev, &error); // ?? number of eigenvalues/eigenvectors?
+elpa_set_integer(handle, "nev", nev, &error); // number of eigenvectors to be calculated (all eigenvalues are calculated regardless this setting)
 if (world_rank==0 || debug_mode) printf("nev=%i is set, error=%i\n", nev, error);
 
 elpa_set_integer(handle, "local_nrows", m_loc, &error); // m_loc
@@ -214,16 +207,17 @@ if (world_rank==0 || debug_mode) printf("process_row=%i is set, error=%i\n", myr
 elpa_set_integer(handle, "process_col", mycol, &error); // mycol
 if (world_rank==0 || debug_mode) printf("process_col=%i is set, error=%i\n", mycol, error);
 
-int error1 = elpa_setup(handle);
-printf("ELPA setup done, error=%i\n", error1);
-if (error1!=ELPA_OK)
+// Finialize the setup of mandatory parameters of the ELPA object (handle)
+status = elpa_setup(handle);
+printf("ELPA setup done, error=%i\n", status);
+if (status!=ELPA_OK)
   {
-  printf("ELPA setup failed, error=%i\n", error1);
+  printf("elpa_setup() failed, error=%i\n", status);
   MPI_Finalize();
   return(1);
   }
-//if(world_rank == 0) printf("ELPA setup done\n");
-// Set ELPA tunables 
+
+// Set ELPA tunables. They can be changed between different ELPA runs, e.g. elpa_eigenvectors() calls
 elpa_set_integer(handle, "solver", ELPA_SOLVER, &error);
 if (world_rank==0 || debug_mode) 
   {
@@ -233,61 +227,66 @@ if (world_rank==0 || debug_mode)
   }
   
 MPI_Barrier(MPI_COMM_WORLD);   
-// End Setup ELPA 
 
-
-// parameters of the matrix
-
-
- 
-// Work only the process in the process grid
-
-double *A_loc, *Z_loc, *work, *Eigenvalues;
-//vector<double> A_loc; // this is the matrix we diagonalize here
-//vector<double> Z_loc, work, Eigenvalues;
+// End of Setup ELPA 
+//____________________________________________ 
 
 
 // Allocate the matrices A_loc, Z_loc(vector of eigenvalues), Eigenvalues
+double *A_loc, *Z_loc, *work, *Eigenvalues;
 A_loc = (double *)calloc(m_loc*n_loc,sizeof(double)) ;
 Z_loc = (double *)calloc(m_loc*n_loc,sizeof(double)) ;
 Eigenvalues = (double *)calloc(N,sizeof(double)) ;
   
 
-// fill the local part of matrix A 
+// Fill the local part of matrix A 
 int k = 0;
 int i_loc, j_loc;
-for(int i_loc=0; i_loc<m_loc; i_loc++) // iteration over the first state
+for(int i_loc=0; i_loc<m_loc; i_loc++) // iteration over rows of A_loc
     {
     int l_1 = i_loc/NB; // local coord of the (NBxNB) block among other blocks
     int x_1 = i_loc%NB; // local coord within the block
     int I_gl= (l_1*nprow + myrow)*NB + x_1; // gl-global; nprow = "P_r"; myrow="p_r"  (quoted-ScaLAPACK userguide notation, p.88-90)
 
-    for(int j_loc=0; j_loc<n_loc; j_loc++) // iteration over the second state
+    for(int j_loc=0; j_loc<n_loc; j_loc++) //  iteration over columns of A_loc
         {
         int l_2 = j_loc/NB; // local coord of the (NBxNB) block among other blocks
         int x_2 = j_loc%NB; // local coord within the block
         int J_gl= (l_2*npcol + mycol)*NB + x_2;
 
+        // ELPA assumes column-major matrix layout
         A_loc[i_loc+j_loc*m_loc] = get_global_matrix_element(I_gl, J_gl);
         }
     }
 
       
-// Initialize the array descriptor for the distributed matrices xA, U and VT
+// Initialize the array descriptors descA and descZ for the distributed matrices A and Z
 itemp = max( 1, m_loc );
 descinit_( descA,  &N, &N, &NB, &NB, &izero, &izero, &ictxt, &itemp, &info );
 descinit_( descZ,  &N, &N, &NB, &NB, &izero, &izero, &ictxt, &itemp, &info );
 
 
+//____________________________________________ 
+// Perform the diagonalization using ELPA
 
 double t_start = MPI_Wtime();
 
-elpa_eigenvectors(handle, A_loc, Eigenvalues, Z_loc, &error); // Eigenvalues=eigenvector matrix, Z_loc =eigenvalue vector
+// A_loc=local part of the matrix to be diagonalized, input
+// Eigenvalues=global array of eigenvalues, output
+// Z_loc=matrix of eigenvectors (contained as columns), output
+elpa_eigenvectors(handle, A_loc, Eigenvalues, Z_loc, &error); 
+
+if (error!=ELPA_OK)
+  {
+  printf("elpa_eigenvectors() failed, error=%i\n", error);
+  MPI_Finalize();
+  return(1);
+  }
 
 double t_stop = MPI_Wtime();
 	
-
-	
+//____________________________________________ 
+// Print the results	
 
 if (world_rank==0)
   {
@@ -309,7 +308,8 @@ if (world_rank==0)
   printf("Diagonalization time (sec): \n%g\n", t_stop-t_start);
   }
 
-// clean up
+//____________________________________________ 
+// Clean up
 
 free(A_loc);
 free(Z_loc);
